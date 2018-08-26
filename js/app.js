b691cd8d9a414be3db6e739364e1bb2e00216f9c
Abby Max <abigaelmae.maximo@gmail.com>
2018-08-25 19:39:59 -0700

    JS: Edited code so that it is more readable   Deleted unnecessary codes Added 1 to final moves - prior to adding, final moves in   modal is minus 1 from final player moves Changed card images - theme: social media
    CSS:
    Edited physical appearance - shape & width of cards

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-/*EDIT CODE SO IT'S MORE READABLE & ADD COMMENTS!!!*/
-
 /*
  * LIST THAT HOLDS ALL CARDS
  *
@@ -7,9 +5,9 @@
  * of each card because the player will be matching them. From the original html,
  * we moved the list of cards to here to create an array.
  */
-const icons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o",
-"fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube",
-"fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"]
+const icons = ["fa fa-youtube-play", "fa fa-youtube-play", "fa fa-twitter", "fa fa-twitter",
+"	fa fa-twitch", "	fa fa-twitch", "fa fa-tumblr-square", "fa fa-tumblr-square", "fa fa-steam-square", "fa fa-steam-square",
+"fa fa-reddit-alien", "fa fa-reddit-alien", "fa fa-instagram", "fa fa-instagram", "fa fa-github-alt", "fa fa-github-alt"]
 
 //Selected the parent deck from index.html. This contained all the cards from the html code
 const cardsContainer = document.querySelector(".deck");
@@ -19,8 +17,7 @@ const cardsContainer = document.querySelector(".deck");
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
-
-
+*/
 //Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -34,7 +31,7 @@ function shuffle(array) {
     }
 
     return array;
-}*/
+}
 
 let openedCards = [];
 let matchedCards = [];
@@ -49,8 +46,7 @@ let matchedCards = [];
  */
 function init() {
 
-
-  //shuffle(icons); //Calls icons (line 4) to shuffle all cards (from shuffle fxn) before game starts
+  //shuffle(icons); //Calls icons to shuffle all cards (from shuffle fxn) before game starts
 
   for(let i = 0; i < icons.length; i++) {        //this will loop through all the available cards
     const card = document.createElement("li");   //created elements
@@ -62,6 +58,17 @@ function init() {
     click(card);
   }
 }
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *   + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *   + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
 
 /*
  * GAME CLICK EVENT
@@ -113,10 +120,8 @@ function compare(currentCard, previousCard) {
     previousCard.classList.add("match");
 
     matchedCards.push(currentCard, previousCard);
-
     openedCards = [];   //resets all opened cards
-
-    congratulations(); //isOver(); //Call 'isOver' when players finds all matches
+    congratulations(); //Call 'congratulations' when players finds all matches
 
   } else {
 
@@ -125,7 +130,7 @@ function compare(currentCard, previousCard) {
       currentCard.classList.remove("open", "show", "disable");
       previousCard.classList.remove("open", "show", "disable");
       openedCards = [];   //resets all opened cards
-    }, 300);
+    }, 400);
   }
   addMove(); //Call 'addMove' to add moves made by the player to move counter
 }
@@ -136,37 +141,15 @@ function compare(currentCard, previousCard) {
  * This fxn is called once the game is over. When called, the game timer will stopTime
  * and an alert (confirm) message will pop up.
 */
-
 function isOver() {
     if(matchedCards.length === icons.length) {
 
       finalTime();  //Call stopTime fxn to stop the timer once the no. of matchedCards
                     //equal the no. of actual cards
-      //playAgain();  //Calls playAgain fxn to show a confim message to the player showing
-                    //game results and asking if user wants to play again
-     congratulations();
+     congratulations(); //Calls congratulations fxn to show modal once game is over
+                        //Modal shows game results and asks if user wants to play again
     }
 }
-
-/*
- * PLAY AGAIN MESSAGE
- *
- * This fxn will show an alert message (confirm) once the player is finished
- * with the game. The message will show the player the game results and asking
- * if he/she whould like to play again. If user presses 'ok', the page will
- * reload and the game will reset. If user presses 'cancel', the page will
- * stay where it is.
-
-
-function playAgain() {
-
-  var message;
-  if (confirm('GAME OVER! You completed the game in '+ timerContainer.innerHTML
-  + ' and ' + movesContainer.innerHTML + ' moves! You get '
-  + starsContainer.innerText + ' stars! Would you like to play again?')) {
-    window.location.reload();
-  }
-} */
 
 /*
  * STAR RATING
@@ -177,10 +160,10 @@ function playAgain() {
  */
 const starsContainer = document.querySelector(".stars");
 const star = `<li><i class="fa fa-star"></i></li>`
-function starRating() { //rating wil be measured by the no. of player moves
+function starRating() { //star rating wil be measured by the no. of player moves
   switch(moves) {
     case 15:
-      starsContainer.innerHTML = star + star;
+      starsContainer.innerHTML = star + " " + star;
     break;
 
     case 20:
@@ -188,15 +171,6 @@ function starRating() { //rating wil be measured by the no. of player moves
     break;
   }
 }
-
-/*
- * RESET STAR RATING
- *
- * This fxn will reset the player's star rating
- */
- function resetStarRating() {
-   starsContainer.innerHTML = star + star + star;
- }
 
 /*
  * ADDING MOVES
@@ -217,22 +191,17 @@ function addMove() {
 }
 
 /*
- * RESET MOVES
+ * TIMER FUNCTIONS
  *
- * This fxn will reset the player's moves made in the game
+ * This function is a counter that will add 1000 miliseconds (1 second) to the
+ * game timer while the player is making card matches.
  */
-function resetMoves() {
-  moves = 0;
-  movesContainer.innerHTML = moves;
-}
-
-//game timer
 var second = 0, minute = 0;
 var timer = document.querySelector(".timer");
 var interval;
 function startTimer(){
     interval = setInterval(function(){
-        timer.innerHTML = minute+" Minutes "+second+" Seconds";
+        timer.innerHTML = minute+" Minute(s) "+second+" Seconds";
         second++;
         if(second == 60){
             minute++;
@@ -242,111 +211,62 @@ function startTimer(){
             hour++;
             minute = 0;
         }
-    },1000);
+    },1000); //1000 miliseconds = 1 second
 }
 
-/*
- * TIMER FUNCTIONS
-*/
 const timerContainer = document.querySelector(".timer");
 let gameTimer,
     totalSeconds = 0; //set starting gameTimer to 0 seconds
     totalMinutes = 0; //set starting gameTimer to 0 minutes
 
 //Set default value to the timer's container
-timerContainer.innerHTML = totalMinutes + ' Minutes ' + totalSeconds + ' Seconds';
+timerContainer.innerHTML = totalMinutes + ' Minute(s) ' + totalSeconds + ' Seconds';
 
 /*
- * START TIMER
+ * GAME MODAL
  *
- * This fxn will start the game timer counting in seconds. Increments will be
- * made in miliseconds (1000 ms = 1 second of gameplay)
-
- function startTimer() {
-    gameTimer = setInterval(function() {
-        totalSeconds++; //Increase totalSeconds by 1
-        timerContainer.innerHTML = totalSeconds + ' Seconds'; //Update game with new time
-    }, 1000); //used 1000 ms beacuse 1 second = 1000 ms
-}
-
-/*
- * STOP TIMER
+ * Modal referenced from: https://scotch.io/tutorials/how-to-build-a-memory-matching-game-in-javascript#7-a-congratulations-modal
  *
- * This fxn will call 'clearInterval' to stop the timer
+ * This modal will be shown once the player makes the last card match. Modal will
+ * let the player know the time he/she took to finish the game, how many moves and
+ * how many stars the player gets. The modal will also ask whether the player
+ * would like to play the game again.
+ */
 
-function finalTime() {
-  clearInterval(gameTimer);
-}*/
-
-/*
- * RESET TIMER
-*/
-function resetTimer() {
-  firstClick = true;
-  totalSeconds = 0; //set starting gameTimer to 0 seconds
-  totalMinutes = 0; //set starting gameTimer to 0 minutes
-  timerContainer.innerHTML = totalMinutes + ' Minutes ' + totalSeconds + " Seconds";
-}
-
-/*
- * RESTART BUTTON
- *
- * Once the player presses the restart button, the game will automatically
- * reset.
- * Full fxn of restart button: All cards on board will be deleted and 'init'
- * is called to create new cards on the board. All past matched cards,
- * counted moves, star rating and timer will reset
-*/
-const restartBtn = document.querySelector(".restart");
-restartBtn.addEventListener("click", function() {
-
-  cardsContainer.innerHTML = "";    //Delete ALL cards
-  init(); //Call 'init' to create new cardsContainer
-  matchedCards = [];  //Reset cards
-  resetMoves(); //Reset moves
-  resetStarRating(); //Reset star rating to 3 stars
-  //finalTime();  // Call 'finalTime' to stop timer
-  resetTimer(); // Call 'resetTimer' to reset timer
-
-});
-
-//modal referenced from https://scotch.io/tutorials/how-to-build-a-memory-matching-game-in-javascript#7-a-congratulations-modal
-//modal
-let modal = document.getElementById("popup1")
-//stars list
- let starsList = document.querySelectorAll(".stars li");
-//close icon in modal
-let closeicon = document.querySelector(".close");
-//congratulations when all cards match, show modal and moves, time and rating
+let modal = document.getElementById("popup1") //modal
+let starsList = document.querySelectorAll(".stars li"); //stars list
+let closeicon = document.querySelector(".close"); //close icon in modal
+//congratulations fxn that will show player's game stats
 function congratulations(){
-    if (matchedCards.length == 16){
+    if (matchedCards.length == 16){   //Once the number of matched cards = 16, the timer will stop
         clearInterval(interval);
         finalTime = timerContainer.innerHTML;
-    //show congratulations modal
-    modal.classList.add("show");
-    //declare star rating variable
-    var starRating = document.querySelector(".stars").innerHTML;
-    //showing move, rating, time on modal
-    document.getElementById("finalMove").innerHTML = moves;
-    document.getElementById("starRating").innerHTML = starRating;
-    document.getElementById("totalTime").innerHTML = finalTime;
-    //closeicon on modal
-    closeModal();
+    modal.classList.add("show");      //show congratulations modal
+    var starRating = document.querySelector(".stars").innerHTML; //declare star rating variable
+    document.getElementById("finalMove").innerHTML = moves + 1; //show/call for number of moves
+    //I added 1 to the final number of moves because the modal did not count the last move
+    //the player made. Without it, the final number of moves will be minus 1 from the true
+    //number of moves
+    document.getElementById("starRating").innerHTML = starRating; //show/call for player's star rating
+    document.getElementById("totalTime").innerHTML = finalTime; //show/call for player's time
+    closeModal(); //closeicon on modal
     };
 }
 
-//close icon on modal
+//Close icon for modal
 function closeModal(){
-    closeicon.addEventListener("click", function(e){
-        modal.classList.remove("show");
+    closeicon.addEventListener("click", function(e){  //Create event listener for close btn
+        modal.classList.remove("show"); //This will close the modal
     });
 }
 
-//for player to play Again
+//Play again button for modal
 function playAgain(){
     button.addEventListener("click", function(e){
     modal.classList.remove("show");
     //window.location.reload()
+      //I was going to leave the reload() command for playAgain on js but decided
+      //to leave it on the on-click line in the html file
   });
 }
 
@@ -354,16 +274,3 @@ function playAgain(){
  * CALL INIT FXN TO START GAME!
  */
 init();
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *   + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *   + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
